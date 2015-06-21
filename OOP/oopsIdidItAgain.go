8571714b3eb4c9fe7f4e5e612f63c16a73dac40f
Gunjan Patel <patelgunjan5@gmail.com>
2015-06-20 23:34:37 -0700

    2

--- OOP/oopsIdidItAgain.go
+++ OOP/oopsIdidItAgain.go
@@ -11,11 +11,28 @@ type Rect struct {
 	hei int
 }
 
+type Rects []Rect
+
 func (r *Rect) Area() int {
 	return r.wid * r.hei
 }
 
+func (rs Rects) Area() int {
+	var a int
+
+	for _, r := range rs {
+		a += r.Area()
+	}
+	return a
+}
+
 func main() {
 	r := Rect{wid: 10, hei: 5}
+	t := Rect{wid: 11, hei: 6}
+	y := Rect{wid: 12, hei: 7}
+
+	rs := Rects{r, t, y}
+
 	fmt.Println("area: ", r.Area())
+	fmt.Println("\n\n area: ", rs.Area())
 }
