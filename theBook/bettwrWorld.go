ed5a68c5134a9e61c735211a8a25bebc7c550589
Gunjan Patel <patelgunjan5@gmail.com>
2015-07-05 03:30:34 -0700

    3

--- theBook/bettwrWorld.go
+++ theBook/bettwrWorld.go
@@ -4,8 +4,20 @@ import . "fmt"
 
 func main() {
 	Println(message())
+	print("yello", "humans")
+	Println("I'm", peek())
+	p := Sprintf("ayy %v", peek())
+	Printf(p)
 }
 
 func message() (string, string) {
 	return "hello", "world"
+}
+
+func peek() (msg string) {
+	msg = "pikachu"
+	return
+}
+func print(v ...interface{}) {
+	Println(v...)
 }
